3e0fd2c1749b6727188445338b5e9a381510c180
Nicholas Buckley <npb630@verizon.net>
2023-11-04 14:39:56 -0400

    debug_to_runners

--- run_arm.sh
+++ run_arm.sh
@@ -1,9 +1,47 @@
 # /bin/bash
+
+MODE=0
+
+usage () {
+    cat << EOF
+Usage: $(basename $0) [-hd] [ARM file]
+    -h: Display help
+    -d: Enter debug mode
+    [ARM file]: .s file
+EOF
+}
+
+while getopts ":hd" options; do
+    case "${options}" in  
+        h)
+            usage
+            exit 0
+            ;;  
+        d)
+            MODE=1
+            ;;
+        *)
+            echo "Error: Unknown option '-"$OPTARG"'." 1>&2
+            usage
+            exit 1
+            ;;        
+    esac
+done
+shift $((OPTIND-1))
+
 WORK_DIR=$( mktemp -d )
 
-aarch64-linux-gnu-as $1 -o $WORK_DIR/temp.o
-aarch64-linux-gnu-ld $WORK_DIR/temp.o -lc -o $WORK_DIR/temp
-qemu-aarch64 -L /usr/aarch64-linux-gnu/ $WORK_DIR/temp
+if [[ MODE -eq 0 ]]; then
+    aarch64-linux-gnu-as -o $WORK_DIR/temp.o $1
+    aarch64-linux-gnu-ld -lc -o $WORK_DIR/temp $WORK_DIR/temp.o 
+    qemu-aarch64 -L /usr/aarch64-linux-gnu/ $WORK_DIR/temp
+else
+    aarch64-linux-gnu-as -o $WORK_DIR/temp.o -g $1
+    aarch64-linux-gnu-ld -lc -o $WORK_DIR/temp $WORK_DIR/temp.o
+    qemu-aarch64 -L /usr/aarch64-linux-gnu/ -g 1234 $WORK_DIR/temp &
+
+    gdb-multiarch --nh -q $WORK_DIR/temp -ex 'set disassemble-next-line on' -ex 'target remote :1234' -ex 'set solib-search-path /usr/aarch64-linux-gnu-lib/' -ex 'layout regs'
+fi
 
 rm -rf "$WORK_DIR"
 exit 0
